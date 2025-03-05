ab98cc20e14c056f4cfe43f0a534d4125cdaad29
AzureAD\KavishMojhoa-Zapproa <kavish@zapproach.com>
2025-03-05 08:17:49 +0400

    button color

--- src/pages/dashboard/tables.jsx
+++ src/pages/dashboard/tables.jsx
@@ -259,7 +259,7 @@ export function Tables({ onExpensesUpdated }) {
         </CardHeader>
         <div className="flex justify-end mb-4 mr-10">
             <Button 
-              className="flex items-center bg-blue-600 text-white px-4 py-2 rounded-md"
+              className="flex items-center bg-[#000000] text-white px-4 py-2 rounded-md"
               onClick={() => openModal()}
             >
               <PlusCircleIcon className="w-5 h-5 mr-2" />
